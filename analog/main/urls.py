085cbdea46e19d1861333550dc60f1a548574595
Oleksii_K <sumyultras88@gmail.com>
2025-01-26 12:44:02 +0200

    GOIT-WEBPY-13-2

--- analog/main/urls.py
+++ analog/main/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views  # Импортируем встроенные представления
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -10,4 +10,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('register/', views.register, name='register'),
     path('logout/', auth_views.LogoutView.as_view(next_page='quote_list'), name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 ]
